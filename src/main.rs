8137314c485b8b0e372acc4fa691ad1eec2ff273
Kyle Schmerge <42156018+kpzip@users.noreply.github.com>
2026-03-01 20:30:54 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,17 @@ mod z80;
 use core::time::Duration;
 use log::info;
 use uefi::prelude::*;
+use crate::memory::{Kaypro2IO, Kaypro2Memory, Z80Memory, Z80IO};
+use crate::z80::Z80;
 
 #[entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
-    info!("Hello, World!");
-    boot::stall(Duration::from_secs(10));
-    Status::SUCCESS
+    let mut z80 = Z80::new();
+    let mut kaypro_memory = Kaypro2Memory::new();
+    let mut kaypro_io = Kaypro2IO::new();
+    loop {
+        z80.clock_cycle(&mut kaypro_memory, &mut kaypro_io);
+        
+    }
 }
